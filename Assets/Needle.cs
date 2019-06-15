3f97a00017d5671a8cd554431b773a429a28f275
imagess1 <50568494+imagess1@users.noreply.github.com>
2019-06-15 12:12:31 +0800

    19.06.15

---
+++ Assets/Needle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Needle : MonoBehaviour
+{
+    private float speed = 15f;
+    private Turntable _Turntable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Turntable = GameObject.Find("Turnatble").GetComponent<Turntable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            transform.parent = collision.transform;
+            speed = 0;
+            _Turntable.OnNumber();
+        }
+        else if (collision.collider.tag == "Need")
+        {
+            Camera.main.backgroundColor = Color.red;
+            _Turntable.OnTriggetr();
+        }
+    }
+}
---
+++ Assets/Turntable.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Turntable : MonoBehaviour
+{
+    public GameObject GO;
+    private int number; //计数
+    private bool isTrigger; //判断游戏是否结束
+    public Transform tran;
+    public Text text;       //显示计数
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTrigger = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTrigger)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(GO, tran.transform.position, Quaternion.identity);
+            }
+            transform.Rotate(Vector3.back, Time.deltaTime * 10f);
+        }
+    }
+
+
+    public void OnNumber()
+    {
+        number++;
+        text.text = number.ToString();
+    }
+
+    public void OnTriggetr()
+    {
+        isTrigger = false;
+    }
+
+}
